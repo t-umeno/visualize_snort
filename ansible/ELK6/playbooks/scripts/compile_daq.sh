8201306756a4439fa74cd4a92a57a13954559e1d
Takashi Umeno <umeno.takashi@gmail.com>
2018-10-06 13:08:56 +0900

    new file:   compile_daq.sh

---
+++ ansible/ELK6/playbooks/scripts/compile_daq.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+( cd $HOME && wget https://snort.org/downloads/snort/daq-${daq_version}.tar.gz && tar zxvpf daq-${daq_version}.tar.gz && (cd daq-${daq_version} && ./configure && make && make install) && rm -r daq-${daq_version} daq-${daq_version}.tar.gz )
