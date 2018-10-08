8cb562e7a2d1d05b0c2b5a7049da8c335e690546
Takashi Umeno <umeno.takashi@gmail.com>
2018-10-08 23:02:07 +0900

    change: don't exec remove snort file

--- ansible/ELK6/playbooks/scripts/compile_snort.sh
+++ ansible/ELK6/playbooks/scripts/compile_snort.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-( cd $HOME && wget https://snort.org/downloads/snort/snort-${snort_version}.tar.gz && tar zxvpf snort-${snort_version}.tar.gz && (cd snort-${snort_version} && ./configure --enable-sourcefire && make && make install) && rm -r snort-${snort_version} snort-${snort_version}.tar.gz )
+( cd $HOME && wget https://snort.org/downloads/snort/snort-${snort_version}.tar.gz && tar zxvpf snort-${snort_version}.tar.gz && (cd snort-${snort_version} && ./configure --enable-sourcefire && make && make install) )
