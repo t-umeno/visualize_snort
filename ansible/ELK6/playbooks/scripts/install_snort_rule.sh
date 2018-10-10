0785c5334eb6188544d58b108e9693816f1fa094
Takashi Umeno <umeno.takashi@gmail.com>
2018-10-10 22:17:01 +0900

    fix: exec groupadd and useradd

--- ansible/ELK6/playbooks/scripts/install_snort_rule.sh
+++ ansible/ELK6/playbooks/scripts/install_snort_rule.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+groupadd snort
+useradd snort -r -s /sbin/nologin -c SNORT_IDS -g snort
 
-mkdir /etc/snort
-mkdir /etc/snort/rules
-mkdir /etc/snort/rules/iplists
-mkdir /etc/snort/preproc_rules
-mkdir /usr/local/lib/snort_dynamicrules
-mkdir /etc/snort/so_rules
+mkdir -p /etc/snort
+mkdir -p /etc/snort/rules
+mkdir -p /etc/snort/rules/iplists
+mkdir -p /etc/snort/preproc_rules
+mkdir -p /usr/local/lib/snort_dynamicrules
+mkdir -p /etc/snort/so_rules
 
 touch /etc/snort/rules/iplists/black_list.rules
 touch /etc/snort/rules/iplists/white_list.rules
 touch /etc/snort/rules/local.rules
 touch /etc/snort/sid-msg.map
 
-mkdir /var/log/snort
-mkdir /var/log/snort/archived_logs
+mkdir -p /var/log/snort
+mkdir -p /var/log/snort/archived_logs
 
 chmod -R 5775 /etc/snort
 chmod -R 5775 /var/log/snort
@@ -25,7 +27,7 @@ chown -R snort:snort /etc/snort
 chown -R snort:snort /var/log/snort
 chown -R snort:snort /usr/local/lib/snort_dynamicrule
 
-(cd ~/snort-${snort_version}/snort-2.9.9.0/etc/ && cp *.conf* /etc/snort && cp *.map /etc/snort && cp *.dtd /etc/snort )
+(cd ~/snort-${snort_version}/etc/ && cp *.conf* /etc/snort && cp *.map /etc/snort && cp *.dtd /etc/snort )
 (cd ~/snort-${snort_version}/src/dynamic-preprocessors/build/usr/local/lib/snort_dynamicpreprocessor/ && cp * /usr/local/lib/snort_dynamicpreprocessor/ )
 
 sed -i "s/include \$RULE\_PATH/#include \$RULE\_PATH/" /etc/snort/snort.conf
